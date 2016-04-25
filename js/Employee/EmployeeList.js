9bb8fd468de4df0324c6a8328c9f5a98eaf2b0e0
xieyuliang <xieyuliang1985@126.com>
2016-04-25 13:31:19 +0800

    [mod]添加表单验证

--- js/Employee/EmployeeList.js
+++ js/Employee/EmployeeList.js
@@ -47,8 +47,22 @@ angular.module("EmployeeList", [])
 
         $scope.initial();
 
-
-    }]);
+        //confirm-Same
+    }])
+   .directive('pwCheck', function () {
+       return {
+           require: 'ngModel',
+           link: function (scope, elem, attrs, ctrl) {
+               var firstPassword = '#' + attrs.pwCheck;
+               elem.add(firstPassword).on('keyup', function () {
+                   scope.$apply(function () {
+                       var v = elem.val() === $(firstPassword).val();
+                       ctrl.$setValidity('pwmatch', v);
+                   });
+               });
+           }
+       };
+   });;
 
 
 angular.bootstrap(angular.element("#EmployeeList"), ["EmployeeList"]);
