938623a542e1ce2b0f6cd371cc55575a4af89f3b
xieyuliang <xieyuliang1985@126.com>
2016-05-02 16:32:19 +0800

    CommitChanges
    CommitChanges

---
+++ CyScada.BLL/BllBindList.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace CyScada.BLL
+{
+    public class BllBindList
+    {
+        private BllEmployee _bllEmployee = new BllEmployee();
+        private BllAuthority _bllAuthority = new BllAuthority();
+        private BllSideMenu _bllSideMenu = new BllSideMenu();
+
+        public BllEmployee BLLEmployee
+        {
+            get { return _bllEmployee; }
+            set { _bllEmployee = value; }
+        }
+
+        public BllAuthority BLLAuthority
+        {
+            get { return _bllAuthority; }
+            set { _bllAuthority = value; }
+        }
+
+        public BllSideMenu BLLSideMenu
+        {
+            get { return _bllSideMenu; }
+            set { _bllSideMenu = value; }
+        }
+
+
+        public DataTable GetUserList()
+        {
+            var dtUser = new DataTable();
+            dtUser.Columns.Add("id");
+            dtUser.Columns.Add("Name");
+
+            var userList = _bllEmployee.GetEmployeeList();
+            foreach (var user in userList)
+            {
+                dtUser.Rows.Add(user.Id, user.Name);
+            }
+            return dtUser;
+        }
+
+
+        public DataTable GetParentMenuList()
+        {
+            var dtParentMenu = new DataTable();
+            dtParentMenu.Columns.Add("id");
+            dtParentMenu.Columns.Add("text");
+
+            var sideMenuList = BLLSideMenu.GetMenuList();
+            foreach (var sideMenu in sideMenuList)
+            {
+                dtParentMenu.Rows.Add(sideMenu.Id, sideMenu.Name);
+            }
+            return dtParentMenu;
+        }
+
+
+
+        public DataTable GetClassList()
+        {
+            var dtClass = new DataTable();
+            dtClass.Columns.Add("id");
+            dtClass.Columns.Add("text");
+
+            var classTable = _bllSideMenu.GetClassList();//&lt;i class='{0}'&gt;&lt;/i&gt;
+            foreach (DataRow dr in classTable.Rows)
+            {
+                dtClass.Rows.Add(dr["Class"],dr["ClassName"]);
+            }
+
+
+            return dtClass;
+        }
+
+        public DataTable GetAuthorityList()
+        {
+            var dtAuth = new DataTable();
+            dtAuth.Columns.Add("id");
+            dtAuth.Columns.Add("text");
+
+            var authList = _bllAuthority.GetAuthorityList().ToList();
+            foreach (var user in authList)
+            {
+                dtAuth.Rows.Add(user.AuthorityId, user.Name);
+            }
+
+            return dtAuth;
+        }
+
+
+
+
+
+
+
+
+
+        public DataTable GetBindList(string bindType,object param)
+        {
+            var methodName = "Get" + bindType;
+            var method = GetType().GetMethod(methodName);
+            if (method == null)
+            {
+                return null;
+            }
+
+            var parameters = method.GetParameters();
+            if (parameters.Any())
+            {
+                return method.Invoke(this, new[] { param }) as DataTable;
+            }
+            return method.Invoke(this, null) as DataTable;
+        }
+
+    }
+}
---
+++ js/SideMenu/SideMenu.js
@@ -0,0 +1,165 @@
+﻿"use strict";
+
+angular.module("SideMenuList", ['viewService'])
+    .controller("SideMenuListController", function ($scope, $http, sideMenuService, bindListService) {
+        $scope.initial = function() {
+
+            sideMenuService.getList()
+                .success(function(data) {
+                    $scope.sideMenuList = data;
+                    $('#ListTable').bootstrapTable('load', data);
+                }).error(function(error) {
+                    alert(error);
+                });
+
+            bindListService.getAuthorityList()
+                .success(function (data) {
+                    $('#infoAuthorityName').select2({
+                        data: data,
+                        placeholder: "请选择一个权限",
+                        theme: "bootstrap"
+                    });
+                    $('#txtAuthorityName').select2({
+                        data: data,
+                        placeholder: "全部",
+                        allowClear: true,
+                        theme: "bootstrap"
+                    });
+                }).error(function(error) {
+                    alert(error);
+                });
+
+            bindListService.getParentMenuList()
+                .success(function(data) {
+                    $('#infoParentName').select2({
+                        data: data,
+                        placeholder: "无父类",
+                        allowClear: true,
+                        theme: "bootstrap"
+                    });
+                }).error(function(error) {
+                    alert(error);
+                });
+
+            bindListService.getClassList()
+                .success(function(data) {
+                    $('#infoClass').select2({
+                        data: data,
+                        placeholder: "请选择一个图标",
+                        theme: "bootstrap",
+                        templateResult: formatState,
+                        templateSelection: formatState
+                    });
+                }).error(function(error) {
+                    alert(error);
+                });
+        };
+
+
+        $scope.Query = function () {
+            if ($scope.sideMenu) {
+                $scope.sideMenu.AuthorityId = $('#txtAuthorityName').val();
+            }
+            var params = $scope.sideMenu;
+            sideMenuService.getList(params)
+                .success(function(data) {
+                    $('#ListTable').bootstrapTable('load', data);
+                }).error(function(error) {
+                    if (error.status == 403) {
+                        window.location.href = "/Account/Login?ReturnUrl=" + window.location.pathname;
+                    } else {
+                        modal.alertMsg("加载失败！");
+                    }
+                });
+        };
+
+        $scope.Add = function() {
+            $scope.info = {
+                title: "新增用户"
+            };
+        };
+
+        $scope.SaveInfo = function () {
+            $scope.info.ParentId = $('#infoParentName').val();
+            $scope.info.AuthorityId = $('#infoAuthorityName').val();
+            $scope.info.Class = $('#infoClass').val();
+            sideMenuService.save($scope.info).success(function(status) {
+                $scope.Query();
+                $('#InfoModal').modal('toggle');
+            }).error(function(error) {
+                alert(error);
+            });
+        };
+
+
+        $scope.DeleteSideMenu = function (id) {
+            sideMenuService.del(id).success(function () {
+                $scope.Query();
+            });
+        };
+
+
+
+        $scope.initial();
+    });
+
+
+angular.bootstrap(angular.element("#SideMenuList"), ["SideMenuList"]);
+
+function formatState(state) {
+    if (!state.id) { return state.text; }
+    var $state = $(
+      '<span><i class ="' + state.element.value.toLowerCase() + '" ></i></span>'
+    );
+    return $state;
+};
+
+
+function rowNumberFormatter(value, row, index) {
+    return '<span>' + (Number(index) + 1) + '</span>';
+}
+
+
+function controlFormatter(value, row, index) {
+    var controlFormat = '<button class="btn btn-default  controlBtn detail" data-target="#InfoModal" data-toggle="modal">修改</button><button class="btn btn-default  controlBtn delete">删除</button>';
+    return controlFormat;
+}
+
+function iconFormatter(value, row, index) {
+    var iconFormat = '<span><i class="' + value + '"></i></span>';
+    return iconFormat;
+}
+
+var operateEvents = {
+    'click .detail': function (e, value, row, index) {
+        var ctrlScope = angular.element('[ng-controller=SideMenuListController]').scope();
+        ctrlScope.info = row;
+        ctrlScope.info.title = "修改目录";
+        ctrlScope.$apply();
+        $('#infoAuthorityName').val(row.AuthorityId).trigger("change");
+        $('#infoParentName').val(row.ParentId).trigger("change");
+        $('#infoClass').val(row.Class).trigger("change");
+        //$('#infoAuthorityName').selectpicker('val', row.AuthorityId);
+
+    },
+    'click .delete': function (e, value, row, index) {
+        var id = row.Id;
+        var ctrlScope = angular.element('[ng-controller=SideMenuListController]').scope();
+        ctrlScope.DeleteSideMenu(id);
+        //$.ajax({
+        //    url: '../api/SideMenu?id=' + id,
+        //    type: 'DELETE',
+        //    success: function (result) {
+        //        angular.element('#btnQuery').triggerHandler('click');
+        //    },
+        //    error: function (error) {
+        //        alert(error);
+        //    }
+        //});
+    }
+
+};
+
+
+
+
