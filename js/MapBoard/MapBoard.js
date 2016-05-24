5f92c3549809514ab56b1d15904aeed785215925
xieyuliang <xieyuliang1985@126.com>
2016-05-24 11:06:26 +0800

    [mod]change MAP settings

--- js/MapBoard/MapBoard.js
+++ js/MapBoard/MapBoard.js
@@ -1,10 +1,10 @@
 ﻿$(document).ready(function () {
     // 百度地图API功能
-    var map = new window.BMap.Map("allmap");
+    var map = new window.BMap.Map("allmap", { enableMapClick: false });
     // 创建Map实例
     map.centerAndZoom(new window.BMap.Point(116.404, 39.915), 11);
     // 初始化地图,设置中心点坐标和地图级别
-    map.addControl(new window.BMap.MapTypeControl());
+    map.addControl(new window.BMap.MapTypeControl({ mapTypes: [window.BMAP_NORMAL_MAP, window.BMAP_SATELLITE_MAP] }));
     //添加地图类型控件
     map.setCurrentCity("北京");
     map.setMapStyle({ style: 'light' });
