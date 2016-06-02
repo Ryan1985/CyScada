e9eff9281e532117ca987b198973fe952bb43d63
xieyuliang <xieyuliang1985@126.com>
2016-06-02 19:25:17 +0800

    add
    commit

---
+++ Controllers/testController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CyScada.Web.Controllers
+{
+    public class testController : Controller
+    {
+        //
+        // GET: /test/
+
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+
+
+        //
+        // GET: /test/
+
+        public ActionResult Detail()
+        {
+            return PartialView();
+        }
+
+    }
+}
