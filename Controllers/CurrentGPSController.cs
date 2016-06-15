fcae9d16a54a9337b3bc9ba5bb86a374922734d1
xieyuliang <xieyuliang1985@126.com>
2016-06-15 22:34:55 +0800

    uploadNewFiles
    uploadNewFiles

---
+++ Controllers/CurrentGPSController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CyScada.Model;
+
+namespace CyScada.Web.Controllers
+{
+    public class CurrentGPSController : Controller
+    {
+        //
+        // GET: /CurrentGPS/
+
+        public ActionResult Index(string sideMenuId)
+        {
+            var user = Session["User"] as UserModel;
+            if (user == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            ViewBag.Id = user.Id;
+            ViewBag.Name = user.Name;
+            ViewBag.SideMenuId = sideMenuId;
+            return View();
+        }
+
+    }
+}
