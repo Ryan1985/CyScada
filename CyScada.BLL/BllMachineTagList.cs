34b9b5d4b926876c86820680d28f9bd747f12273
xieyuliang <xieyuliang1985@126.com>
2016-07-22 11:40:18 +0800

    CommitMaster
    CommitMaster

---
+++ CyScada.BLL/BllMachineTagList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using CyScada.DAL;
+
+namespace CyScada.BLL
+{
+    public class BllMachineTagList
+    {
+        private DalMachineTag _dalMachineTag = new DalMachineTag();
+
+        public DalMachineTag DalMachineTag
+        {
+            get { return _dalMachineTag; }
+            set { _dalMachineTag = value; }
+        }
+
+
+        public DataTable GetAllTags()
+        {
+            return _dalMachineTag.GetTags();
+        }
+
+
+
+    }
+}
