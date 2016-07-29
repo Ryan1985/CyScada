97e79a240ed628ed433423c27c95e1a7993e3da5
2837473201 <2837473201@qq.com>
2016-07-29 17:33:50 +0800

    报表先提交以后再更改
    报表先提交以后再更改

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CyScada.Model;
+
+namespace CyScada.Web.Controllers
+{
+    public class ReportController : Controller
+    {
+        //
+        // GET: /History/
+
+        public ActionResult Index(string MenuId)
+        {
+            var user = Session["User"] as UserModel;
+            if (user == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            ViewBag.Id = user.Id;
+            ViewBag.Name = user.Name;
+            ViewBag.SideMenuId = MenuId;
+            return PartialView();
+        }
+
+    }
+}
