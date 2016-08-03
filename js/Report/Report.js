08c16692aca2bc60de145846986e5e95fdbff9a6
2837473201 <2837473201@qq.com>
2016-08-03 14:46:49 +0800

    生产报表
    生产报表

--- js/Report/Report.js
+++ js/Report/Report.js
@@ -1,178 +1,127 @@
 ﻿"use strict";
 var dataSet = [
-    ["Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800"],
-    ["Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750"],
-    ["Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000"],
-    ["Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "6224", "2012/03/29", "$433,060"],
-    ["Airi Satou", "Accountant", "Tokyo", "5407", "2008/11/28", "$162,700"],
-    ["Brielle Williamson", "Integration Specialist", "New York", "4804", "2012/12/02", "$372,000"],
-    ["Herrod Chandler", "Sales Assistant", "San Francisco", "9608", "2012/08/06", "$137,500"],
-    ["Rhona Davidson", "Integration Specialist", "Tokyo", "6200", "2010/10/14", "$327,900"],
-    ["Colleen Hurst", "Javascript Developer", "San Francisco", "2360", "2009/09/15", "$205,500"],
-    ["Sonya Frost", "Software Engineer", "Edinburgh", "1667", "2008/12/13", "$103,600"],
-    ["Jena Gaines", "Office Manager", "London", "3814", "2008/12/19", "$90,560"],
-    ["Quinn Flynn", "Support Lead", "Edinburgh", "9497", "2013/03/03", "$342,000"],
-    ["Charde Marshall", "Regional Director", "San Francisco", "6741", "2008/10/16", "$470,600"],
-    ["Haley Kennedy", "Senior Marketing Designer", "London", "3597", "2012/12/18", "$313,500"],
-    ["Tatyana Fitzpatrick", "Regional Director", "London", "1965", "2010/03/17", "$385,750"],
-    ["Michael Silva", "Marketing Designer", "London", "1581", "2012/11/27", "$198,500"],
-    ["Paul Byrd", "Chief Financial Officer (CFO)", "New York", "3059", "2010/06/09", "$725,000"],
-    ["Gloria Little", "Systems Administrator", "New York", "1721", "2009/04/10", "$237,500"],
-    ["Bradley Greer", "Software Engineer", "London", "2558", "2012/10/13", "$132,000"],
-    ["Dai Rios", "Personnel Lead", "Edinburgh", "2290", "2012/09/26", "$217,500"],
-    ["Jenette Caldwell", "Development Lead", "New York", "1937", "2011/09/03", "$345,000"],
-    ["Yuri Berry", "Chief Marketing Officer (CMO)", "New York", "6154", "2009/06/25", "$675,000"],
-    ["Caesar Vance", "Pre-Sales Support", "New York", "8330", "2011/12/12", "$106,450"],
-    ["Doris Wilder", "Sales Assistant", "Sidney", "3023", "2010/09/20", "$85,600"],
-    ["Angelica Ramos", "Chief Executive Officer (CEO)", "London", "5797", "2009/10/09", "$1,200,000"],
-    ["Gavin Joyce", "Developer", "Edinburgh", "8822", "2010/12/22", "$92,575"],
-    ["Jennifer Chang", "Regional Director", "Singapore", "9239", "2010/11/14", "$357,650"],
-    ["Brenden Wagner", "Software Engineer", "San Francisco", "1314", "2011/06/07", "$206,850"],
-    ["Fiona Green", "Chief Operating Officer (COO)", "San Francisco", "2947", "2010/03/11", "$850,000"],
-    ["Shou Itou", "Regional Marketing", "Tokyo", "8899", "2011/08/14", "$163,000"],
-    ["Michelle House", "Integration Specialist", "Sidney", "2769", "2011/06/02", "$95,400"],
-    ["Suki Burks", "Developer", "London", "6832", "2009/10/22", "$114,500"],
-    ["Prescott Bartlett", "Technical Author", "London", "3606", "2011/05/07", "$145,000"],
-    ["Gavin Cortez", "Team Leader", "San Francisco", "2860", "2008/10/26", "$235,500"],
-    ["Martena Mccray", "Post-Sales support", "Edinburgh", "8240", "2011/03/09", "$324,050"],
-    ["Unity Butler", "Marketing Designer", "San Francisco", "5384", "2009/12/09", "$85,675"]
+    ["2016-07-01", "350", "25", "中文", "30", "300"],
+    ["2016-07-02", "400", "233", "8422", "53", "170"],
+    ["2016-07-03", "287", "367", "1562", "45", "86"],
+    ["2016-07-04", "70", "256", "6224", "12", "433"],
+    ["2016-07-05", "90", "236", "5407", "23", "162"],
+    ["2016-07-06", "60", "236", "4804", "45", "372"],
+    ["2016-07-07", "95", "571", "9608", "78", "137"],
+    ["2016-07-08", "239", "236", "6200", "78", "327"],
+    ["2016-07-09", "480", "712", "2360", "88", "205"],
+    ["2016-07-10", "365", "239", "1667", "23", "103"],
+    ["2016-07-11", "116", "247", "3814", "45", "90"],
+    ["2016-07-12", "456", "236", "9497", "32", "342"],
+    ["2016-07-13", "326", "236", "6741", "22", "470"],
+    ["2016-07-14", "231", "235", "3597", "99", "313"],
+    ["2016-07-15", "567", "222", "1965", "56", "385"],
+    ["2016-07-16", "124", "333", "1581", "88", "198"],
+    ["2016-07-17", "674", "331", "3059", "35", "725"],
+    ["2016-07-18", "127", "321", "1721", "66", "237"],
+    ["2016-07-19", "985", "223", "2558", "23", "132"],
+    ["2016-07-20", "235", "282", "2290", "34", "217"],
+    ["2016-07-21", "125", "378", "1937", "32", "345"],
+    ["2016-07-22", "236", "237", "6154", "27", "675"],
+    ["2016-07-23", "236", "237", "8330", "83", "106"],
+    ["2016-07-24", "236", "236", "3023", "92", "85"],
+    ["2016-07-25", "523", "222", "5797", "42", "120"],
+    ["2016-07-26", "216", "890", "8822", "26", "925"],
+    ["2016-07-27", "321", "222", "9239", "88", "350"],
+    ["2016-07-28", "642", "123", "1314", "24", "200"],
+    ["2016-07-29", "271", "356", "2947", "45", "850"],
+    ["2016-07-30", "218", "865", "8899", "45", "160"],
+    ["2016-07-31", "851", "961", "2769", "47", "950"]
 ];
 $(document).ready(function () {
-    $('#example').DataTable({
+    var datatables=$('#example').DataTable({
+        language:{
+            "sProcessing":"处理中...",
+
+            "sLengthMenu":"显示_MENU_项结果",
+
+            "sZeroRecords":"没有匹配结果",
+
+            "sInfo":"第_START_至_END_条结果，共_TOTAL_条","sInfoEmpty":"第0至0条结果，共0条",
+
+            "sInfoFiltered":"(由_MAX_项结果过滤)",
+
+            "sInfoPostFix":"",
+
+            "sSearch":"搜索:",
+
+            "sUrl":"",
+
+            "sEmptyTable":"表中数据为空",
+
+            "sLoadingRecords":"载入中...",
+
+            "sInfoThousands":",",
+
+            "oPaginate":{
+
+                "sFirst":"首页",
+
+                "sPrevious":"上页",
+
+                "sNext":"下页",
+
+                "sLast":"末页"
+
+            },
+
+            "oAria":{
+
+                "sSortAscending":":以升序排列此列",
+
+                "sSortDescending":":以降序排列此列"
+
+            }
+        },
         data: dataSet,
         columns: [
-            { title: "Name" },
-            { title: "Position" },
-            { title: "Office" },
-            { title: "Extn." },
-            { title: "Start date" },
-            { title: "Salary" }
+            { title: "日期" },
+            { title: "起重重量&nbsp;&nbsp;(吨)" },
+            { title: "运行时间&nbsp;&nbsp;(分钟)" },
+            { title: "起重次数" },
+            { title: "摇摆次数" },
+            { title: "耗电量&nbsp;&nbsp;(度)" }
+        ],
+       // "bStateSave": true,
+        "sScrollX": "100%",
+        "bScrollCollapse": true,
+        "iDisplayLength": 31,
+       // "pageLength":31,
+        "bLengthChange": false,
+        dom: 'Bfrtip',
+        buttons: [{
+            extend: 'excelHtml5',
+            text: '导出 Excel',
+            customize: function (xlsx) {
+                var sheet = xlsx.xl.worksheets['sheet1.xml'];
+
+                $('row c[r^="C"]', sheet).attr('s', '2');
+            }
+        },
+         {
+             extend: 'csv',
+             text: '导出 CSV',
+             exportOptions: {
+                 modifier: {
+                     search: 'none'
+                 }
+             }
+         },
+         {
+             extend: 'pdf',
+             text: '导出 pdf'
+         },
+         {
+             extend: 'print',
+             text:'打印'
+         }
         ]
-    });
-});
-
-
-
-//"use strict";
-
-//angular.module("History", ['mgcrea.ngStrap', 'viewService'])
-//    .controller("HistoryController", function($scope, $http, bindListService, historyService) {
-//        $scope.initial = function() {
-//            $scope.condition = {};
-
-
-//            $('#dtStartDate').datetimepicker({
-//                minView: 2,
-//                language: "zh-CN",
-//                autoclose:true
-//            });
-//            $('#dtEndDate').datetimepicker({
-//                minView: 2,
-//                language: "zh-CN",
-//                autoclose: true
-//            });
-
-
-
-//        };
-
-//        bindListService.getMachineList([sideMenuId, $('#userId').attr('data-userid')]).success(function (data) {
-//            $('#selMachine').select2({
-//                data: data,
-//                placeholder: "全部",
-//                allowClear: true,
-//                theme: "bootstrap"
-//            });
-//        });
-
-//        historyService.getMachineTagList([sideMenuId, $('#userId').attr('data-userid'), $('#selMachine').val()]).success(function (data) {
-//            $('#selMachineTags').select2({
-//                data: data,
-//                placeholder: "全部",
-//                allowClear: true,
-//                theme: "bootstrap"
-//            });
-//        });
-
         
-
-//        $scope.Query = function () {
-//            $scope.condition.MachineId = $('#selMachine').val();
-//            $scope.condition.DeviceName = $('#selMachineTags').val();
-//            console.log($scope.condition);
-//            if ($scope.condition.MachineId == "全部") {
-//                $scope.condition.MachineId = ""; 
-//            }
-//            if ($scope.condition.DeviceName == "全部") {
-//                $scope.condition.DeviceName = "";
-//            }
-//            $scope.condition.StartDate = $("#dtStartDate").val();
-//            $scope.condition.EndDate = $("#dtEndDate").val();
-//            historyService.getList($scope.condition).success(function (data) {
-//                $('#ListTable').bootstrapTable('load', data);
-//                bindChart(data);
-//            });
-//        };
-
-
-
-//        $scope.initial();
-
-//    });
-
-//angular.bootstrap(angular.element("#History"), ["History"]);
-
-//firstQuery();
-//function firstQuery() {
-//    var curTimeF = new Date().getTime();
-//    var endTimeF = moment(curTimeF).format("YYYY-MM-DD");
-//    var startTimeF = moment(curTimeF).subtract("2", "month").format("YYYY-MM-DD");
-//    console.log(endTimeF, startTimeF);
-//    $("#dtStartDate").val(startTimeF);
-//    $("#dtEndDate").val(endTimeF);
-//    $("#btnQuery").trigger("click");
-//}
-
-//function rowNumberFormatter(value, row, index) {
-//    return '<span>' + (Number(index) + 1) + '</span>';
-//}
-
-//function bindChart(chartData) {
-//    var yAxises = [];
-
-//    for (var i = 0; i < chartData.length; i++) {
-//        yAxises.push([new Date(chartData[i].TimeStamp), Number(chartData[i].Value)]);
-//    }
-
-//    var startDate = new Date(chartData[0].TimeStamp);
-
-//    $('#Chart').highcharts({
-//        chart: { zoomType: 'x', spacingRight: 20 },
-//        title: { text: '历史数据查询', x: -20 },
-//        credits: {enabled: false},
-//        exporting: {enabled: false},
-//        xAxis: {
-//            type: 'datetime',
-//            dateTimeLabelFormats: {
-//                day: '%Y-%b-%e %H:%M:%S'
-//            }
-//        },
-//        yAxis: {
-//            title: { text: '历史记录' },
-//            plotLines: [{ value: 0, width: 1, color: '#808080' }]
-//        },
-//        tooltip: {
-//             valueSuffix: ''
-//        },
-//        legend: {
-//             layout: 'vertical', align: 'right', verticalAlign: 'middle', borderWidth: 0
-//        },
-//        series: [{
-//            name: '压力(kg)', data: yAxises,
-//            pointStart: Date.UTC(startDate.getFullYear(), startDate.getMonth(), startDate.getDate(), startDate.getHours(), startDate.getMinutes(), startDate.getSeconds()),
-//            pointInterval: 1000
-//        }]
-//    });
-
-//}
+    });
+    
+});
 
